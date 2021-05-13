3ec3155c508829d9ef851025c37d5e117cf51c6e
jingle_seven <xiong_jinhua@foxmail.com>
2021-05-13 15:17:55 +0800

    update age60

---
+++ jingle/demo/work/Age60TreatmentListNew.py
@@ -0,0 +1,38 @@
+﻿# coding: utf-8
+# 每月农保待遇核定名单，用来通知到龄人员来开始领取待遇，txt转excel，删掉无效人名
+
+import openpyxl,time
+import jingle.demo.work.SkData as SkData
+
+tableHead = ['序号','村委','姓名','公民身份号码','性别','已缴费月数','当前缴费档次','是否购买职工社保']
+monthStr = time.strftime("%Y年%m月", time.localtime())
+xlsName = monthStr + '农保待遇名单'
+dir = 'C:/Users/Administrator/Desktop/农保/农保每月待遇核定/'
+rawXsl = openpyxl.load_workbook(dir + monthStr + '导出.xlsx')
+sheet0 = rawXsl['sheet0']
+
+wbook = openpyxl.Workbook()
+# sheet = wbook.create_sheet('sheet1'，0) # 新建工作表（表名，位置）
+# for sheet in wb:
+resSheet = wbook['Sheet'] # 使用默认的Sheet
+resSheet.title = xlsName # 更改表名
+resSheet.append(tableHead)
+resSheet2 = wbook.create_sheet('sheet1',1)
+resSheet2.append(tableHead)
+
+rowNum = 2
+paidCount = 1
+notPaidCount = 1
+for x in sheet0.values:
+    # print(x)
+    if x[4] == '未领取' and x[5] == '参保缴费': #
+        rowNum  = rowNum + 1
+        sex = '女' if (int(x[1][-2]))/2==0 else '男'
+        row = [rowNum-2,x[7].replace('村委会',''),x[3],x[1][0:-6]+'***',sex,'','','否']
+        print(row)
+        resSheet.append(row)
+        resSheet2.append([rowNum-2,x[7].replace('村委会',''),x[3],x[1],sex,'','','否'])
+
+SkData.setBorderWidth(resSheet, 25) # 设置边框直到25行
+SkData.setBorderWidth(resSheet2, 25) # 设置边框直到25行
+wbook.save(dir + xlsName + '.xlsx')
--- jingle/test/TimeTest.py
+++ jingle/test/TimeTest.py
@@ -4,34 +4,34 @@ import calendar
 
 # 时间戳
 timeStamp = time.time()
-print(timeStamp)
+print('时间戳',timeStamp)
 
 # 标准时间元组
 localTime = time.localtime()
-print(localTime)
+print('标准时间元组',localTime)
 
 # 日期时间加减
 dt = datetime.datetime.now()
 print(dt,dt.day)
 dt = dt + datetime.timedelta(days=1)
-print(dt,dt.strftime("%Y-%m-%d"))
+print('日期时间加减',dt,dt.strftime("%Y-%m-%d"))
 
 # 字符串转datetime
 dt2 = datetime.datetime.strptime('20200129','%Y%m%d')
-print(dt2)
+print('字符串转datetime',dt2)
 
 # 简单的可视时间 Thu Feb  6 10:09:08 2020
 ascTime = time.asctime()
-print(ascTime)
+print('简单的可视时间',ascTime)
 
 # 格式化时间
 strTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-print(strTime)
+print('格式化时间',strTime)
 
 # 格式化时间转时间戳
 timeArray = time.strptime('09-15-2017 17:53:45', "%m-%d-%Y %H:%M:%S")
 timestamp = time.mktime(timeArray)
-print(timestamp)
+print('格式化时间转时间戳',timestamp)
 
 # 输出一个月的日历
 cal = calendar.month(2016, 10)
@@ -39,4 +39,3 @@ print(cal)
 
 print ("本地时间为 :", time.asctime(),time.ctime())
 print ("格式化的时间 :",time.strftime("%Y-%m-%d  %H:%M:%S", time.localtime()))
-print ("以下输出2016年1月份的日历:\n",calendar.month(2016, 1))
