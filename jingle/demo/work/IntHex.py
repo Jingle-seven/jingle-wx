92005bd7226aa27f6e72b80875d52a143295b380
jingle_seven <xiong_jinhua@foxmail.com>
2021-06-08 11:25:55 +0800

    add 婚前10问

--- jingle/demo/work/IntHex.py
+++ jingle/demo/work/IntHex.py
@@ -15,10 +15,27 @@
 # print(a)
 # s = '123456789'
 # print(s.replace('1','').replace('2',''))
-s = ['a','b','c','d','e',]
-for i in range(0,len(s)):
-    print(i)
-for i in s:
-    if i == 'c' or i == 'b' :
-        s.pop()
-    print(i)
+# s = ['a','b','c','d','e',]
+# for i in range(0,len(s)):
+#     print(i)
+# for i in s:
+#     if i == 'c' or i == 'b' :
+#         s.pop()
+#     print(i)
+def isErrorIdNumber(idNum):
+    if not idNum or '身份证' in idNum: return False # 如果是空，视为正确身份证号码
+    factor = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
+    checkCode = [1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+    idList = ["10" if x in ['X', 'x', 'Ⅹ'] else x for x in list(idNum)]
+    sum = 0
+    try:
+        for i in range(17):
+            sum = sum + int(idList[i]) * factor[i]
+    except Exception as e:
+        return True # 如果是不是数字，视为错误身份证号码
+    if checkCode[sum % 11] == int(idList[17]): # 如果加和结果与最后一位校验码相同，说明是正确的身份证号码
+        return False
+    print('错误的身份证号：',idList,checkCode[sum % 11])
+    return True
+
+isErrorIdNumber('440223199408300516')
